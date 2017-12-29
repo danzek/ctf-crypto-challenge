04cce8f1bebc67932df472e4557d7e7bd08795fb
Dan <d@4n68r.com>
2017-12-29 02:18:45 -0600

    working code but broken logic

--- main.go
+++ main.go
@@ -1,26 +1,47 @@
 // detect type of encryption used given salted hash and decrypt using provided salt
 // path to word list must be provided as command-line parameter
 
+/*
+Copyright © 2017 Dan O'Day (d@4n68r.com)
+
+This work is free. You can redistribute it and/or modify it under the terms of the Do What The Fuck You Want To Public
+License, Version 2, as published by Sam Hocevar. See the COPYING file for more details.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
 // todo -- allow command-line parameter to specify order of salt:hash (is salt first or second?)
 
 package main
 
 import (
-	//"crypto/md5"
-	//"crypto/sha1"
-	//"crypto/sha256"
-	//"crypto/sha512"
+	"crypto/md5"
+	"crypto/sha1"
+	"crypto/sha256"
+	"crypto/sha512"
 	"fmt"
 	"strings"
-	// "path/filepath"
 	"os"
 	"crypto_ctf_challenge/wordlist"
+	"bufio"
 )
+
+var hashSizes = map[int]string {
+	md5.Size: "MD5",
+	sha1.Size: "SHA1",
+	sha256.Size: "SHA256",
+	sha512.Size: "SHA512",
+}
 
 func main() {
 	// establish path to word list
+	var wordListPath string
+
 	if len(os.Args) > 2 {
-		wordListPath := os.Args[1]
+		wordListPath = os.Args[1]
 
 		// for testing
 		if wordListPath == "crackme" {
@@ -41,18 +62,66 @@ func main() {
 		showUsage()
 	}
 
-	// populate map of salted hashes (key is hash, value is salt)
-	hashSaltMap := make(map[string]string)
+	// crack passwords
+	// todo -- use goroutines / concurrency
 	for _, sh := range os.Args[2:] {
-		splitString := strings.Split(sh, ":")
-		if len(splitString) == 2 {
-			hashSaltMap[splitString[1]] = splitString[0]
+		// iterate over word list and crack password
+		if word, ok := crack(sh, wordListPath); ok {
+			fmt.Printf("CRACKED: %s\n", word)
 		} else {
-			fmt.Fprintf(os.Stderr, "Invalid salt:hash -- %s\n", sh)
+			fmt.Fprintf(os.Stderr, "ERROR: %s\n", word)
 		}
 	}
 }
 
 func showUsage() {
-	fmt.Fprint(os.Stderr, "Usage: ./crack /path/to/word/list.txt salt:hash [salt:hash ...]\n")
+	fmt.Fprint(os.Stderr, "Usage: go run main.go /path/to/word/list.txt salt:hash [salt:hash ...]\n")
+}
+
+func crack(saltedHash string, wordListPath string) (string, bool) {
+	splitString := strings.Split(saltedHash, ":")
+	if len(splitString) == 2 {
+		salt := splitString[0]
+		hash := splitString[1]
+
+		if hashType, ok := hashSizes[len(hash)/2]; ok {
+			// iterate over word list
+			f, err := os.Open(wordListPath)
+			if err != nil {
+				fmt.Fprint(os.Stderr, "Error opening word list\n")
+				panic(err)
+			}
+			defer f.Close()
+			scanner := bufio.NewScanner(f)
+			scanner.Split(bufio.ScanLines)
+			for scanner.Scan() {
+				word := strings.TrimSpace(scanner.Text())
+				saltedWord := []byte(fmt.Sprintf("%s:%s", salt, word))
+
+				// calculate hashResult
+				var hashResult string
+				switch hashType {
+				case "MD5":
+					hashResult = fmt.Sprintf("%x", md5.Sum(saltedWord))
+				case "SHA1":
+					hashResult = fmt.Sprintf("%x", sha1.Sum(saltedWord))
+				case "SHA256":
+					hashResult = fmt.Sprintf("%x", sha256.Sum256(saltedWord))
+				case "SHA512":
+					hashResult = fmt.Sprintf("%x", sha512.Sum512(saltedWord))
+				}
+
+				// compare hashResult to saltedHash
+				if hashResult == saltedHash {
+					return word, true
+				}
+			}
+		} else {
+			return "UNRECOGNIZED HASH TYPE", false
+		}
+	} else {
+		return "INVALID SALT:HASH", false
+	}
+
+	return "WORD NOT FOUND", false
 }
